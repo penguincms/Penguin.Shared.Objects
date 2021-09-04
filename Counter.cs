3de5ea5dd1bac4a667e35958744a0751f3e1f843
MrJackSpade <loffredo.ap@gmail.com>
2021-09-04 12:40:13 -0700

    Create Counter.cs

---
+++ Counter.cs
@@ -0,0 +1,31 @@
+﻿namespace Penguin.Shared.Objects
+{
+    public class Counter
+    {
+        private readonly object _lock = new object();
+
+        public int Increment()
+        {
+            lock (_lock)
+            {
+                _Value++;
+                return _Value;
+            }
+
+        }
+
+        public static implicit operator Counter(int v)
+        {
+            return new Counter(v);
+        }
+
+        private int _Value;
+
+        public int Value => Value;
+
+        public Counter(int value)
+        {
+            _Value = value;
+        }
+    }
+}
